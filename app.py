576b993b1e1cf6601926eb62988616ec09e587a6
Yiqiao Yin <eagle0504@gmail.com>
2023-06-29 14:38:49 -0400

    page updated

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # Adjust the width of the Streamlit page
-st.set_page_config(
-    page_title="Use Pygwalker In Streamlit",
-    layout="wide"
-)
- 
+st.set_page_config(page_title="W.Y.N. Data Viz ⭐", layout="wide")
+
 # Add Title
-st.title("Use Pygwalker In Streamlit")
+st.title("W.Y.N. Data Viz ⭐")
 
 # Insert a file uploader that accepts multiple files at a time
 uploaded_file = st.file_uploader("Choose a CSV file")
 if uploaded_file is not None:
+    # Success message
+    st.success("File uploaded successfully.")
+
     # Can be used wherever a "file-like" object is accepted:
     df = pd.read_csv(uploaded_file)
 
@@ -23,3 +23,5 @@ if uploaded_file is not None:
 
     # Embed the HTML into the Streamlit app
     components.html(pyg_html, height=1000, scrolling=True)
+else:
+    st.warning("Please upload a csv file.")
