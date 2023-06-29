84a9a3da3e9bb7e538bfc18999254d73f113b36a
Yiqiao Yin <eagle0504@gmail.com>
2023-06-29 14:32:47 -0400

    handle data upload exception

--- app.py
+++ app.py
@@ -11,19 +11,19 @@ st.set_page_config(
  
 # Add Title
 st.title("Use Pygwalker In Streamlit")
- 
- # Insert a file uploader that accepts multiple files at a time
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+
+# Insert a file uploader that accepts multiple files at a time
+uploaded_file = st.file_uploader("Choose a CSV file")
+if uploaded_file is not None:
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
 
 # Import your data
 df = pd.read_csv("https://kanaries-app.s3.ap-northeast-1.amazonaws.com/public-datasets/bike_sharing_dc.csv")
- 
+
 # Generate the HTML using Pygwalker
 pyg_html = pyg.walk(df, return_html=True)
- 
+
 # Embed the HTML into the Streamlit app
 components.html(pyg_html, height=1000, scrolling=True)
